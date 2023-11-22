50c6d22af25e27247f721f3b06ff2dea4e591fa0
Franck <franckvienot7@gmail.com>
2023-11-22 16:24:56 +0100

    started building new project on top of nextjs

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center p-24">
+        <h1>This is the about page</h1>
+        <button className="btn_primary">Cliquez ici</button>
+      </main>
+    </>
+  )
+}
